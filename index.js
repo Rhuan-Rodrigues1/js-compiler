a5823a0256ed788d8da1fb37620b6b374e0941a4
Rhuan-Rodrigues1 <gitrhuan34@gmail.com>
2023-09-12 15:40:31 -0300

    init project

---
+++ index.js
@@ -0,0 +1,17 @@
+import antlr4 from "antlr4";
+import ECMAScriptLexer from "./lib/ECMAScriptLexer.js";
+import ECMAScriptParser from "./lib/ECMAScriptParser.js";
+
+const input = "{x: 1}";
+
+const chars = new antlr4.InputStream(input);
+const lexer = new ECMAScriptLexer(chars);
+
+//lexer.strictMode = false;
+
+const tokens = new antlr4.CommonTokenStream(lexer);
+const parser = new ECMAScriptParser(tokens);
+
+const tree = parser.expressionSequence();
+
+console.log(tree.toStringTree(parser.ruleNames));
